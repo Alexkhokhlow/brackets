bfd176cac1af7e0f90964e5ccc4e80a5b99902da
Alexey KHokhlow <lomtik25@gmail.com>
2021-02-22 13:24:24 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let startKeys = []
+    let endKeys = []
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        startKeys.push(bracketsConfig[j][0])
+        endKeys.push(bracketsConfig[j][1])
+    }
+    let masItem = str.split('')
+    let start = 0
+    for (let i = 0; i < masItem.length; i++) {
+
+        start = startKeys.indexOf(masItem[i])
+        if (start != -1) {
+            if (endKeys[start] == masItem[i + 1]) {
+                masItem.splice(i, 2)
+                i = i - 2
+            }
+        }
+
+    }
+
+    return masItem.length == 0 ? true : false
+
 }
